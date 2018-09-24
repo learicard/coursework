4905b411b54b5b8f3e24f03cfc5dc3977f74ca8b
Joseph Viviano <joseph@viviano.ca>
2018-09-24 01:34:45 -0400

    almost done...

--- comp550/assignment01/a1q3p3.py
+++ comp550/assignment01/a1q3p3.py
@@ -8,17 +8,47 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.svm import LinearSVC
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import KFold
-from sklearn.metrics import log_loss
+from sklearn.model_selection import KFold, RandomizedSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import log_loss, accuracy_score
+
+from scipy import stats
 
 import nltk
-from nltk import word_tokenize
+#from nltk import word_tokenize
 from nltk.corpus import stopwords
 
 NEGFILE = 'rt-polarity.neg'
 POSFILE = 'rt-polarity.pos'
-FOLDS = 10
 
+FOLDS = 10
+INNER = 3
+N_CV = 100
+
+MIN_DF = stats.uniform(10e-10, 10e-4)
+NGRAMS = ((1, 1), (1, 2), (2, 2))
+STOPWS = (set(stopwords.words('english')), None)
+
+SETTINGS_NB = {
+    'pre__min_df': MIN_DF,
+    'pre__ngram_range': NGRAMS,
+    'pre__stop_words': STOPWS,
+    'clf__alpha': stats.uniform(0.5, 2)
+}
+
+SETTINGS_SVC = {
+    'pre__min_df': MIN_DF,
+    'pre__ngram_range': NGRAMS,
+    'pre__stop_words': STOPWS,
+    'clf__C': stats.uniform(10e-3, 100),
+    'clf__kernel': 'linear'
+}
+SETTINGS_LR = {
+    'pre__min_df': MIN_DF,
+    'pre__ngram_range': NGRAMS,
+    'pre__stop_words': STOPWS,
+    'clf__C': stats.uniform(10e-3, 100),
+}
 
 def get_data():
     """
@@ -43,7 +73,21 @@ def get_data():
     return(neg_data, y)
 
 
-def preprocess(X, ngmin=1, ngmax=2, rmstop=False, threshold=1):
+#def preprocess(X, ngmin=1, ngmax=2, rmstop=False, threshold=1):
+#    if rmstop:
+#        to_rm = stopwords.words('english')
+#    else:
+#        to_rm = None
+#
+#    vectorizer = CountVectorizer(
+#        ngram_range=(ngmin, ngmax), stop_words=to_rm, min_df=threshold)
+#
+#    X = vectorizer.fit_transform(X)
+#
+#    return(X)
+
+
+def classify(X, y, clf_type='nb'):
     """
     Preprocess the input documents to extract feature vector representations of
     them. Your features should be N-gram counts, for N<=2.
@@ -53,47 +97,62 @@ def preprocess(X, ngmin=1, ngmax=2, rmstop=False, threshold=1):
     2. Experiment with removing stop words. (see NLTK)
     3. Remove infrequently occurring words and bigrams as features. You may tune
        the threshold at which to remove infrequent words and bigrams.
+    4. Search over hyperparameters for the three models (nb, svm, lr) to
+       find the best performing model.
+
+    All 4 of the above are done in the context of 10-fold cross validation on
+    the data. On the training data, 3-fold cross validation is done to find the
+    optimal hyperparameters (using randomized CV), which are then tested on
+    held-out data.
     """
-
-    if rmstop:
-        to_rm = stopwords.words('english')
-    else:
-        to_rm = None
-
-    vectorizer = CountVectorizer(
-        ngram_range=(ngmin, ngmax), stop_words=to_rm, min_df=threshold)
-
-    X = vectorizer.fit_transform(X)
-
-    return(X)
-
-
-def classify(X, y, clf_type='nb'):
 
     if clf_type == 'nb':
         clf = BernoulliNB()
+        params = SETTINGS_NB
     elif clf_type == 'svm':
         clf = LinearSVC()
+        params = SETTINGS_SVC
     elif clf_type == 'lr':
         clf = LogisticRegression()
+        params = SETTINGS_LR
     else:
         raise Exception('{} is an invalid clf: {nb, svm, lr}'.format(clf_type))
 
-    losses = []
+    pipe = Pipeline([
+        ('pre', CountVectorizer()),
+        ('clf', clf),
+    ])
+
+    model = RandomizedSearchCV(pipe, params, n_jobs=-1, n_iter=N_CV, cv=INNER)
+
+    test_results = {'loss': [], 'accuracy': []}
+    cv_results = {}
+
     kf = KFold(n_splits=FOLDS, shuffle=True)
+    X = np.array(X) # convert so we can use indexing
+
+    i = 0
     for train_idx, test_idx in kf.split(y):
-        X_train = X[train_idx, :]
-        X_test = X[test_idx, :]
+        i += 1
+        print("[{}/{}] Fold for model {}".format(i, FOLDS, clf_type))
+        X_train = X[train_idx]
+        X_test = X[test_idx]
         y_train = y[train_idx]
         y_test = y[test_idx]
 
-        clf.fit(X_train, y_train)
-        y_pred = clf.predict(X_test)
+        model.fit(X_train, y_train)
+        best_params = model.best_estimator_.get_params()
 
-        loss = log_loss(y_test, y_pred)
-        losses.append(loss)
+        for p in sorted(params.keys()):
+            cv_results[p] = best_params[p]
+            #print("\t%s: %r" % (p, best_params[p]))
 
-    return(np.mean(losses), np.std(losses))
+        y_pred = model.predict(X_test)
+
+        test_results['loss'].append(log_loss(y_test, y_pred))
+        test_results['accuracy'].append(accuracy_score(y_test, y_pred))
+
+    return(test_results, cv_results)
 
 
 def main():
@@ -101,11 +160,13 @@ def main():
     X, y = get_data()
 
     # calls to preprocess define ngrams, stop word removal, and threshold
-    X = preprocess(X, ngmin=1, ngmax=2, rmstop=False, threshold=1)
+    #X = preprocess(X, ngmin=1, ngmax=2, rmstop=False, threshold=1)
 
-    mean, std = classify(X, y)
+    nb_test_results, nb_cv_results = classify(X, y, clf_type='nb')
+    sv_test_results, sv_cv_results = classify(X, y, clf_type='svc')
+    lr_test_results, lr_cv_results = classify(X, y, clf_type='lr')
 
-    print('{}+/-{}'.format(mean, std))
+    import IPython; IPython.embed()
 
 if __name__ == '__main__':
     main()
